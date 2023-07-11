28d3d2ef4842fb1548b6588515ca19d2b33f4fe2
ParvathiAjay <pajay@jord.com.au>
2023-07-11 16:10:44 +0530

    test

--- src/App.js
+++ src/App.js
@@ -1,21 +1,33 @@
-import React from 'react';
-import { BrowserRouter as Router,Routes, Route } from 'react-router-dom';
-import Home from './components/Home';
-import About from './components/About';
-import Help from './components/Help';
-// import Contact from './components/Contact';
+// import React from 'react';
+// import { BrowserRouter as Router,Routes, Route } from 'react-router-dom';
+// import Home from './components/Home';
+// import About from './components/About';
+// import Help from './components/Help';
+// // import Contact from './components/Contact';
+
+// const App = () => {
+//   return (
+//     <Router basename='/React_JWT'>
+//       <Routes>
+//         <Route path="/" element={<Home />} />
+//         {/* <Route path="/contact" element={<Contact />} /> */}
+//         <Route path="/about" element={<About />} />
+//         <Route path="/help" element={<Help />} />
+//       </Routes>
+//     </Router>
+//   );
+// };
+
+// export default App;
+
+import React from "react";
 
 const App = () => {
-  return (
-    <Router basename='/React_JWT'>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        {/* <Route path="/contact" element={<Contact />} /> */}
-        <Route path="/about" element={<About />} />
-        <Route path="/help" element={<Help />} />
-      </Routes>
-    </Router>
-  );
+    return(
+        <div>
+          Hello Welcome
+        </div>
+    );
 };
 
 export default App;
