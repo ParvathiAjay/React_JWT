85b693e21f1a4fe2aab37f5717febed793589c54
ParvathiAjay <pajay@jord.com.au>
2023-07-11 16:27:23 +0530

    test

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,7 +4,7 @@ import { Link, Routes, Route } from 'react-router-dom';
 const Home = () => {
   return (
     <div>
-      <h1>Home</h1>
+      <h2>Home</h2>
       <Link to="/about">Go to About</Link>
     </div>
   );
