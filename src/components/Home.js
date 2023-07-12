dfa50e3590dfa4256d7a010f28f3d6b1784db0b5
ParvathiAjay <pajay@jord.com.au>
2023-07-12 12:37:51 +0530

    update

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,22 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Link, Routes, Route } from 'react-router-dom';
+import axios from 'axios';
 
 const Home = () => {
+  useEffect(() => {
+    axios
+      .get(`${process.env.REACT_APP_API_URL}` + '/test')
+      .then((res) => {
+        console.log('res');
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  });
   return (
     <div>
       <h2>Home</h2>
-      <Link to="/about">Go to About</Link> <br/> <br/>
+      <Link to="/about">Go to About</Link> <br />
       {process.env.REACT_APP_API_URL}
     </div>
   );
